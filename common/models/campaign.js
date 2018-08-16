e40257e602d50a6c23f95e2fd57ac3127225ec72
Eugene <shakuskys@gmail.com>
2018-08-16 22:39:30 +0300

    membership method

--- common/models/campaign.js
+++ common/models/campaign.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Campaign) {
+  Campaign.membership = function(adventurerId, cb) {
+    Campaign.find({where: {members: {inq: adventurerId}}},
+      function(err, instance) {
+        var response = instance;
+        cb(null, response);
+      }
+    );
+  };
 
+  Campaign.remoteMethod(
+    'membership',
+    {
+      http: {path: '/membership', verb: 'get'},
+      accepts: {arg: 'adventurerId', type: 'string', http: {source: 'query'}},
+      returns: {arg: 'campaigns', type: 'object'},
+    }
+  );
 };
