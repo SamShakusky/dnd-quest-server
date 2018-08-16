3c3295f2bf8af5004546de2ff69e62d487ccaebb
Eugene <shakuskys@gmail.com>
2018-08-17 00:41:36 +0300

    addMember method

--- common/models/campaign.js
+++ common/models/campaign.js
@@ -2,6 +2,7 @@
 
 module.exports = function(Campaign) {
   Campaign.membership = function(adventurerId, cb) {
+    console.log(adventurerId);
     Campaign.find({where: {members: {inq: adventurerId}}},
       function(err, instance) {
         var response = instance;
@@ -15,6 +16,26 @@ module.exports = function(Campaign) {
     {
       http: {path: '/membership', verb: 'get'},
       accepts: {arg: 'adventurerId', type: 'string', http: {source: 'query'}},
+      returns: {arg: 'campaigns', type: 'object'},
+    }
+  );
+
+  Campaign.addMember = function(campaignId, adventurerId, cb) {
+    Campaign.findById(campaignId, function(err, instance) {
+      instance.updateAttributes({$push: {members: adventurerId}});
+      cb(null, instance);
+    });
+  };
+
+  Campaign.remoteMethod(
+    'addMember',
+    {
+      http: {path: '/:id/addMember', verb: 'put'},
+      accepts: [
+        {arg: 'id', type: 'string', description: 'campaignId'},
+        {arg: 'adventurerId', type: 'string'},
+      ],
+      description: 'Adds an Adventurer as a campaign member',
       returns: {arg: 'campaigns', type: 'object'},
     }
   );
