863c080c0460ed99631d3a60a9d15c85b1dd9fe5
Eugene <shakuskys@gmail.com>
2018-09-29 11:16:38 +0300

    client build

--- server/middleware/redirect404.js
+++ server/middleware/redirect404.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 module.exports = function() {
-    console.log('fired!');
   return function redirect404(req, res, next) {
     var newPath = path.normalize(__dirname + '/../../client/index.html');
     res.sendFile(newPath);
