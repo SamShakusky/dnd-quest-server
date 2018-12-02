fc8211702b33e1a72932f1c28f84621824587680
Eugene <shakuskys@gmail.com>
2018-12-02 16:22:56 +0300

    tester set and remove methods

--- common/models/party.js
+++ common/models/party.js
@@ -3,8 +3,8 @@
 module.exports = function(Party) {
   const send = require('../lib/mailer');
 
-  Party.observe('before save', function sendEmails(ctx, next) {
-    const emailList = ctx.instance.emails;
+  Party.afterRemote('create', function sendEmails(ctx, modelInstance, next) {
+    const emailList = modelInstance.emails;
 
     emailList.forEach(email => {
       const emailData = {
@@ -18,4 +18,61 @@ module.exports = function(Party) {
 
     next();
   });
+
+  Party.tester = function(partyId, flag, cb) {
+    Party.findById(partyId, function(err, instance) {
+      if (err || !instance) return cb(null, err);
+
+      instance.updateAttributes({tester: flag}, function(err, changedInstance) {
+        if (err || !changedInstance) return cb(null, err);
+
+        cb(null, changedInstance);
+      });
+    });
+  };
+
+  Party.remoteMethod(
+    'tester',
+    {
+      http: {path: '/:id/tester', verb: 'put'},
+      accepts: [
+        {arg: 'id', type: 'string', description: 'partyId'},
+        {arg: 'flag', type: 'boolean'},
+      ],
+      description: 'Changes Party\'s testing status',
+      returns: {arg: 'party', type: 'object'},
+    }
+  );
+
+  Party.removeTesters = function(cb) {
+    Party.find({where: {tester: true}},
+      function(err, parties) {
+        if (err || !parties) return cb(null, err);
+        console.log(parties);
+
+        var results = parties.map(instance => {
+          return instance.updateAttributes({tester: false},
+            function(err, changedInstance) {
+              if (err || !changedInstance) return cb(null, err);
+
+              return changedInstance;
+            });
+        });
+
+        Promise.all(results).then((completed) => {
+          return cb(null, completed);
+        });
+
+        cb(null, parties);
+      });
+  };
+
+  Party.remoteMethod(
+    'removeTesters',
+    {
+      http: {path: '/removeTesters', verb: 'put'},
+      description: 'Changes all Parties\' testing status to false',
+      returns: {arg: 'parties', type: 'array'},
+    }
+  );
 };
