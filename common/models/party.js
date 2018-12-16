76ffb25cdc84598d3d07af570315527f40b50575
Eugene <shakuskys@gmail.com>
2018-12-16 16:15:04 +0300

    random testers status

--- common/models/party.js
+++ common/models/party.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = function(Party) {
+  var app = require('../../server/server');
+  var Adventurer = app.models.Adventurer;
   const send = require('../lib/mailer');
+  const randomList = require('../lib/random-list');
 
   Party.afterRemote('create', function sendEmails(ctx, modelInstance, next) {
     const emailList = modelInstance.emails;
@@ -44,11 +47,34 @@ module.exports = function(Party) {
     }
   );
 
+  Party.addTesters = function(cb) {
+    Party.find({}, function(err, parties) {
+      const randomIndexes = randomList(5, parties.length - 1);
+      parties.map((party, i) => {
+        if (randomIndexes.indexOf(i) !== -1) {
+          party.updateAttributes({tester: true},
+            function(err, changedInstance) {
+              if (err || !changedInstance) return cb(null, err);
+            });
+        }
+      });
+      cb(null, parties);
+    });
+  };
+
+  Party.remoteMethod(
+    'addTesters',
+    {
+      http: {path: '/addTesters', verb: 'post'},
+      description: 'Randomly add tester status to Parties',
+      returns: {arg: 'parties', type: 'array'},
+    }
+  );
+
   Party.removeTesters = function(cb) {
     Party.find({where: {tester: true}},
       function(err, parties) {
         if (err || !parties) return cb(null, err);
-        console.log(parties);
 
         var results = parties.map(instance => {
           return instance.updateAttributes({tester: false},
@@ -60,7 +86,9 @@ module.exports = function(Party) {
         });
 
         Promise.all(results).then((completed) => {
-          return cb(null, completed);
+          return completed;
+        }).catch((err) => {
+          return err;
         });
 
         cb(null, parties);
@@ -75,4 +103,40 @@ module.exports = function(Party) {
       returns: {arg: 'parties', type: 'array'},
     }
   );
+
+  Party.createAdventures = function(cb) {
+    Party.find({where: {tester: true}},
+      function(err, parties) {
+        if (err || !parties) return cb(null, err);
+
+        Adventurer.create([
+          {username: '1', email: '1@dawdd.com', password: '1234'},
+          {username: '2', email: '2@2.aa', password: '1234'},
+        ], function(err, adventurers) {
+          let result = [];
+          if (err) {
+            adventurers.map((adv, i) => {
+              if (err[i] != null) {
+                adventurers[i].err = {
+                  name: err[i].name,
+                  messages: err[i].details.messages,
+                };
+              }
+            });
+          }
+
+          return cb(null, adventurers);
+        });
+      });
+  };
+
+  Party.remoteMethod(
+    'createAdventures',
+    {
+      http: {path: '/createAdventures', verb: 'post'},
+      description: 'Create Adventurers by Party testing status',
+      returns: {arg: 'adventurers', type: 'object'},
+    }
+  );
 };
+
