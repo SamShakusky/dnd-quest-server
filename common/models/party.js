d1fa59c2ee96f31bf749cb5a97d855f4310c3ec1
Eugene <shakuskys@gmail.com>
2018-12-25 13:23:19 +0300

    createAdventures remote method

--- common/models/party.js
+++ common/models/party.js
@@ -5,35 +5,36 @@ module.exports = function(Party) {
   var Adventurer = app.models.Adventurer;
   const send = require('../lib/mailer');
   const randomList = require('../lib/random-list');
-
+  const tempPass = require('../../config/keys/tempPass');
+  
   Party.afterRemote('create', function sendEmails(ctx, modelInstance, next) {
     const emailList = modelInstance.emails;
-
+    
     emailList.forEach(email => {
       const emailData = {
         to: email,
         subject: 'Successful registration',
         html: '<h3 style="color: red;">Well done!</h3>',
       };
-
+      
       // send(emailData);
     });
-
+    
     next();
   });
-
+  
   Party.tester = function(partyId, flag, cb) {
     Party.findById(partyId, function(err, instance) {
       if (err || !instance) return cb(null, err);
-
+      
       instance.updateAttributes({tester: flag}, function(err, changedInstance) {
         if (err || !changedInstance) return cb(null, err);
-
+        
         cb(null, changedInstance);
       });
     });
   };
-
+  
   Party.remoteMethod(
     'tester',
     {
@@ -46,7 +47,7 @@ module.exports = function(Party) {
       returns: {arg: 'party', type: 'object'},
     }
   );
-
+  
   Party.addTesters = function(amount, cb) {
     Party.find({}, function(err, parties) {
       const randomIndexes = randomList(+amount, parties.length - 1);
@@ -61,7 +62,7 @@ module.exports = function(Party) {
       cb(null, parties);
     });
   };
-
+  
   Party.remoteMethod(
     'addTesters',
     {
@@ -73,31 +74,31 @@ module.exports = function(Party) {
       returns: {arg: 'parties', type: 'array'},
     }
   );
-
+  
   Party.removeTesters = function(cb) {
     Party.find({where: {tester: true}},
       function(err, parties) {
         if (err || !parties) return cb(null, err);
-
+        
         var results = parties.map(instance => {
           return instance.updateAttributes({tester: false},
             function(err, changedInstance) {
               if (err || !changedInstance) return cb(null, err);
-
+              
               return changedInstance;
             });
         });
-
+        
         Promise.all(results).then((completed) => {
           return completed;
         }).catch((err) => {
           return err;
         });
-
+        
         cb(null, parties);
       });
   };
-
+  
   Party.remoteMethod(
     'removeTesters',
     {
@@ -106,39 +107,91 @@ module.exports = function(Party) {
       returns: {arg: 'parties', type: 'array'},
     }
   );
-
+  
   Party.createAdventures = function(cb) {
     Party.find({where: {tester: true}},
       function(err, parties) {
         if (err || !parties) return cb(null, err);
-
-        Adventurer.create([
-          {username: '1', email: '1@dawdd.com', password: '1234'},
-          {username: '2', email: '2@2.aa', password: '1234'},
-        ], function(err, adventurers) {
-          let result = [];
-          if (err) {
-            adventurers.map((adv, i) => {
-              if (err[i] != null) {
-                adventurers[i].err = {
-                  name: err[i].name,
-                  messages: err[i].details.messages,
-                };
-              }
+        
+        // Arrays for the response
+        const totalAdventurers = [];
+        const totalCampaigns = [];
+        const totalErrors = [];
+        
+        // Iterate through tester parties
+        const results = parties.map((party) => {
+          if (!party || !party.emails) return false;
+          const partyMembers = [];
+          
+          party.emails.map(email => {
+            const mailUser = email.match(/^([^@]*)@/)[1];
+            const username = mailUser + Math.random().toString().slice(-4);
+            partyMembers.push({
+              username,
+              email,
+              password: tempPass, // every tester is obliged to change this password
             });
-          }
-
-          return cb(null, adventurers);
+          });
+          
+          // At the end we'll get new adventurers with a campaign
+          return Adventurer.create(partyMembers)
+            .then(adventurers => {
+              // Adventurers were created successfuly, pass their data to response
+              totalAdventurers.push(adventurers);
+              
+              const advIds = adventurers
+                .map(adv => adv.id.toString());
+              
+              // Now we need a campaign for the adventurers
+              // The first one would be it's owner
+              return adventurers[0].campaigns.create({
+                title: `${adventurers[0].username}'s Campaign`,
+                members: advIds,
+              }).then(campaign => {
+                // Returning the new campaign to pass it in a response
+                return campaign;
+              }).catch(err => {
+                return cb(err);
+              });
+            }).catch(error => {
+              error.map((err, i) => {
+                if (err != null) {
+                  // Adventurer wasn't created for some reason
+                  totalErrors.push(err.message);
+                }
+              });
+              
+              // nor was the campaign, so null would be passed to the response instead
+              return null;
+            });
+        });
+        
+        Promise.all(results).then((campaigns) => {
+          // passing campaigns' data to the response
+          // including nulls, if there were some errors
+          totalCampaigns.push(campaigns);
+          
+          return cb(null, {
+            totalAdventurers,
+            totalCampaigns,
+            totalErrors,
+          });
+        }).catch(err => {
+          return cb(err);
         });
       });
   };
-
+  
   Party.remoteMethod(
     'createAdventures',
     {
       http: {path: '/createAdventures', verb: 'post'},
       description: 'Create Adventurers by Party testing status',
-      returns: {arg: 'adventurers', type: 'object'},
+      returns: [
+        {arg: 'totalAdventurers', type: 'array'},
+        {arg: 'totalCampaigns', type: 'array'},
+        {arg: 'totalErrors', type: 'array'},
+      ],
     }
   );
 };
