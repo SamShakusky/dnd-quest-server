e947729f022241403c60f0ae3aa8e40f36c5c87b
Eugene <shakuskys@gmail.com>
2018-12-26 13:21:11 +0300

    setPassword method

--- common/models/adventurer.js
+++ common/models/adventurer.js
@@ -1,5 +1,36 @@
 'use strict';
 
 module.exports = function(Adventurer) {
-
+  Adventurer.on('resetPasswordRequest', function(data) {
+    console.log(data);
+  });
+  
+  Adventurer.setPassword = function(cb) {
+    Adventurer.find().then(adventurers => {
+      const results = adventurers.map(adventurer => {
+        return Adventurer.resetPassword({
+          email: adventurer.email,
+        }).then(() => {
+          return adventurer.email;
+        }).catch(err => {
+          throw new Error(err);
+        });
+      });
+      
+      Promise.all(results).then((result) => {
+        return cb(null, result);
+      }).catch(err => {
+        return cb(err);
+      });
+    });
+  };
+  
+  Adventurer.remoteMethod(
+    'setPassword',
+    {
+      http: {path: '/setPassword', verb: 'post'},
+      description: 'Set new password',
+      returns: {arg: 'successful', type: 'array'},
+    }
+  );
 };
