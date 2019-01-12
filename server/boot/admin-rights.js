74edb32ddd55fea033bd8ed1e10bb6e3e0cb9df6
Eugene <shakuskys@gmail.com>
2019-01-12 13:15:16 +0300

    creating user to grant rights

--- server/boot/admin-rights.js
+++ server/boot/admin-rights.js
@@ -5,40 +5,52 @@
 //   var Role = app.models.Role;
 //   var RoleMapping = app.models.RoleMapping;
   
-//   Adventurer.find({where: {email: '123'}})
+//   Adventurer.find({where: {email: '-'}})
 //     .then(adventurer => {
-//       if (!adventurer.length) {
-//         console.log('User does not exist');
-//         return false;
-//       }
+//       let newAdmin = adventurer[0];
       
-//       Role.find({where: {name: 'admin'}})
-//         .then(role => {
-//           if (!role.length) {
-//             Role.create({name: 'admin'})
-//               .then(role => {
-//                 console.log('Created role:', role);
-                
-//                 giveAdminRights(adventurer[0], role);
-//               });
-//           }
+//       if (!newAdmin) {
+//         Adventurer.create([
+//           {username: '-', email: '-', password: '-'},
+//         ]).then(newAdventurer => {
+//           console.log('User was created');
+//           newAdmin = newAdventurer[0];
           
-//           RoleMapping.find({where: {roleId: role[0].id}})
-//             .then(adminRoles => {
-//               const alreadyAdmin = adminRoles.some(adRole => {
-//                 return adRole.principalId == adventurer[0].id;
-//               });
-//               if (alreadyAdmin) {
-//                 console.log(
-//                   `The user ${adventurer[0].username} already has admin rights!`
-//                 );
-//               } else {
-//                 giveAdminRights(adventurer[0], role[0]);
-//               }
-//             });
+//           setRole(newAdmin);
 //         });
+//       } else {
+//         setRole(newAdmin);
+//       }
 //     });
-    
+  
+//   function setRole(newAdmin) {
+//     Role.find({where: {name: 'admin'}})
+//       .then(role => {
+//         if (!role.length) {
+//           Role.create({name: 'admin'})
+//             .then(role => {
+//               console.log('Created role:', role);
+              
+//               giveAdminRights(newAdmin, role);
+//             });
+//         }
+        
+//         RoleMapping.find({where: {roleId: role[0].id}})
+//           .then(adminRoles => {
+//             const alreadyAdmin = adminRoles.some(adRole => {
+//               return adRole.principalId == newAdmin.id;
+//             });
+//             if (alreadyAdmin) {
+//               console.log(
+//                 `The user ${newAdmin.username} already has admin rights!`
+//               );
+//             } else {
+//               giveAdminRights(newAdmin, role[0]);
+//             }
+//           });
+//       });
+//   }
+  
 //   function giveAdminRights(adventurer, role) {
 //     role.principals.create({
 //       principalType: RoleMapping.USER,
